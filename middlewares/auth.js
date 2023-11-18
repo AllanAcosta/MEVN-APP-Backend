0217167c7a882b3cdda4aca0d07ab75f2cd65921
Allan Acosta <46501625+AllanAcosta@users.noreply.github.com>
2023-11-18 09:17:15 -0500

    Deleted Verify Seller Method

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,6 @@
 import tokenService from "../services/token"
 
 export default {
-  verifyUser: async (req, res, next) => {
-    if (!req.headers.token) {
-      return res.status(404).send({
-        message: "No Token",
-      })
-    }
-    const response = await tokenService.decode(req.headers.token)
-    if (
-      response.role == "Administrator" ||
-      response.role == "Client" ||
-      response.role == "Seller"
-    ) {
-      next()
-    } else {
-      return res.status(403).send({
-        message: "Not Authorized",
-      })
-    }
-  },
   verifyAdmin: async (req, res, next) => {
     if (!req.headers.token) {
       return res.status(404).send({
@@ -35,29 +16,15 @@ export default {
       })
     }
   },
-  verifyClient: async (req, res, next) => {
+  verifyUser: async (req, res, next) => {
     if (!req.headers.token) {
       return res.status(404).send({
         message: "No Token",
       })
     }
     const response = await tokenService.decode(req.headers.token)
-    if (response.role == "Administrator" || response.role == "Client") {
-      next()
-    } else {
-      return res.status(403).send({
-        message: "Not Authorized",
-      })
-    }
-  },
-  verifySeller: async (req, res, next) => {
-    if (!req.headers.token) {
-      return res.status(404).send({
-        message: "No Token",
-      })
-    }
-    const response = await tokenService.decode(req.headers.token)
-    if (response.role == "Administrator" || response.role == "Seller") {
+
+    if (response.role == "Administrator" || response.role == "User") {
       next()
     } else {
       return res.status(403).send({
